0287a4228cd37b1da3a75d154b06265fba5d6e8a
Colin Rolfe <crolfe@gmail.com>
2013-03-06 08:32:42 +1100

    Removed group, user and file sections from tomcat module and let package decide what's best

--- modules/tomcat/manifests/init.pp
+++ modules/tomcat/manifests/init.pp
@@ -1,33 +1,6 @@
 class tomcat {
 
-    group { 'tomcat6':   
-        ensure => present,
-        gid => '1003',
-    }
-
-    user { 'tomcat6':
-        ensure => present,
-        gid => 'tomcat',
-        groups => ['tomcat6'],
-        require => [ Group['tomcat6'], Package['tomcat6'] ],
-    }
-
-    package { 'tomcat6':
+     package { 'tomcat6':
         ensure => "installed",
      }
-
-    file { '/var/lib/tomcat6/webapps':
-        ensure  => 'directory',
-        group   => 'tomcat6',
-        owner   => 'tomcat6',
-        purge   => true,
-        recurse => true,
-        force   => true,
-        require => Package['tomcat6'],
-  }
-
-  service { "tomcat6":
-    ensure => "running",
-    require => Package["tomcat6"],
-  }
 }
