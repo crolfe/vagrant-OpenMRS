b49d23ca5cf33fc271d04acd8b92a1a270cbd68f
Steph Hof <steph.hof@gmail.com>
2013-04-23 20:22:25 +1000

    Changing the openMRS module (war download directory) so we don't download it every vagrant up/provision

--- Vagrantfile
+++ Vagrantfile
@@ -32,7 +32,7 @@ Vagrant::Config.run do |config|
   # Share an additional folder to the guest VM. The first argument is
   # an identifier, the second is the path on the guest to mount the
   # folder, and the third is the path on the host to the actual folder.
-   config.vm.share_folder "code", "/vagrant/code", "code"
+  config.vm.share_folder "code", "/vagrant/code", "code"
 
   # Set Vagrant to use Puppet for provisioning and where to find the manifests/modules
   config.vm.provision :puppet do |puppet|
--- modules/openmrs/manifests/init.pp
+++ modules/openmrs/manifests/init.pp
@@ -8,7 +8,7 @@ define download($uri, $timeout = 300) {
 
 class openmrs {
   $version = '1.9.3'
-  $stage = "/tmp/openmrs-${version}.war"
+  $stage = "/var/tmp/openmrs-${version}.war"
   $target = '/var/lib/tomcat6/webapps/openmrs.war'
 
   file { '/tmp':
@@ -19,20 +19,20 @@ class openmrs {
     uri => "http://downloads.sourceforge.net/project/openmrs/releases/OpenMRS_${version}/openmrs.war",
     timeout => 900,
     require => File["/tmp"],
-    
+
   }
 
-    file { '/var/lib/tomcat6/webapps':
-        ensure => "directory",
-        owner => "tomcat6",
-        group => "tomcat6",
-        require => Package["tomcat6"],
-    }
+  file { '/var/lib/tomcat6/webapps':
+    ensure => "directory",
+    owner => "tomcat6",
+    group => "tomcat6",
+    require => Package["tomcat6"],
+  }
 
-    file { $target:
-        ensure  => present,
-        require => [File['/var/lib/tomcat6/webapps'], Download["$stage"], ],
-        source  => $stage,
+  file { $target:
+    ensure  => present,
+    require => [File['/var/lib/tomcat6/webapps'], Download["$stage"], ],
+    source  => $stage,
   }
 
   file { "/usr/share/tomcat6/.OpenMRS":
